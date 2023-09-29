03cc1525ff72f0c97ba388720abc40565819a9c3
Essel Luna <essel.luna1@trans-cosmos.co.jp>
2023-09-29 14:27:47 +0800

    update js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-// $(window).on('load', function () {
-//     $('.accordionList .accordionQuestion').click(function() {
-//         var $accordionHead = $(this);
-//         // Open and Close accordion
-//         $accordionHead.next('.accordionPanel').slideToggle();
-
-//         // After a sec, arrow icon will change
-//         setTimeout(function() {
-//             $accordionHead.toggleClass('open');
-//         }, 100); // 1000 is 1 second
-//     });
-// });
-
 const accordionTitles = document.querySelectorAll(".accordionQuestion");
 
 accordionTitles.forEach((accordionQuestion) => {
@@ -26,27 +13,3 @@ accordionTitles.forEach((accordionQuestion) => {
 		}
 	});
 });
-
-// const questions = document.querySelectorAll(".question");
-
-// questions.forEach((question) => {
-//   const button = question.querySelector(".question__button");
-//   const questionText = question.querySelector(".question__title__text");
-//   button.addEventListener("click", () => {
-//     questions.forEach((item) => {
-//       if(item !== question) {
-//         item.classList.remove("show-text");
-//       }
-//     });
-//     question.classList.toggle("show-text");
-//   });
-
-//   questionText.addEventListener("click", () => {
-//     questions.forEach((item) => {
-//       if(item !== question) {
-//         item.classList.remove("show-text");
-//       }
-//     });
-//     question.classList.toggle("show-text");
-//   });
-// });
