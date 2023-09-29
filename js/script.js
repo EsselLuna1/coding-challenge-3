b40b3870875e1d6b8af42fccfce648c3837b2d74
Essel Luna <essel.luna1@trans-cosmos.co.jp>
2023-09-29 14:28:33 +0800

    update const into var in js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-const accordionTitles = document.querySelectorAll(".accordionQuestion");
+var accordionTitles = document.querySelectorAll(".accordionQuestion");
 
 accordionTitles.forEach((accordionQuestion) => {
 	accordionQuestion.addEventListener("click", () => {
 		if (accordionQuestion.classList.contains("is-open")) {
 			accordionQuestion.classList.remove("is-open");
 		} else {
-			const accordionTitlesWithIsOpen = document.querySelectorAll(".is-open");
+			var accordionTitlesWithIsOpen = document.querySelectorAll(".is-open");
 			accordionTitlesWithIsOpen.forEach((accordionTitleWithIsOpen) => {
 				accordionTitleWithIsOpen.classList.remove("is-open");
 			});
